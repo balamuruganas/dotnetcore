a7ee854a6695b59312353f4514f3bceb398501d5
Balamurugan Srinivasan <balamurugan.srinivasan@photoninfotech.net>
2018-11-08 19:41:03 +0530

    Publishing Targets

--- master/Seaknots.TCMS/Seaknots.TCMS.API/Program.cs
+++ master/Seaknots.TCMS/Seaknots.TCMS.API/Program.cs
@@ -1,41 +1,46 @@
-﻿using Microsoft.AspNetCore;
+﻿using System.IO;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Seaknots.TCMS.API
 {
-  public class Program
-  {
-    public static void Main(string[] args)
+    public class Program
     {
-      BuildWebHost(args).Run();
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var host = WebHost.CreateDefaultBuilder(args)
+
+                .UseStartup<Startup>()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .ConfigureAppConfiguration((builderContext, config) =>
+                {
+                    IHostingEnvironment env = builderContext.HostingEnvironment;
+
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                })
+                .Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetService<DbInitializer>();
+                context.Initialize();
+            }
+
+            return host;
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                   .UseStartup<Startup>();
     }
-
-    public static IWebHost BuildWebHost(string[] args)
-    {
-      var host = WebHost.CreateDefaultBuilder(args)
-          .UseStartup<Startup>()
-          .ConfigureAppConfiguration((builderContext, config) =>
-          {
-            IHostingEnvironment env = builderContext.HostingEnvironment;
-
-            config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                      .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
-          })
-          .Build();
-
-      using (var scope = host.Services.CreateScope())
-      {
-        var context = scope.ServiceProvider.GetService<DbInitializer>();
-        context.Initialize();
-      }
-
-      return host;
-    }
-
-    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-        WebHost.CreateDefaultBuilder(args)
-               .UseStartup<Startup>();
-  }
 }
