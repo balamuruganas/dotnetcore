2fa7ec6e1a92633cffc827bb3c4412a8b478b7d2
Balamurugan Srinivasan <balamurugan.srinivasan@photoninfotech.net>
2018-11-21 17:04:21 +0530

    Cors

--- master/Seaknots.TCMS/Seaknots.TCMS.API/DbInitializer.cs
+++ master/Seaknots.TCMS/Seaknots.TCMS.API/DbInitializer.cs
@@ -6,61 +6,61 @@ using Seaknots.TCMS.Entities;
 
 namespace Seaknots.TCMS.API
 {
-  public class DbInitializer
-  {
-    private readonly TCMSContext _ctx;
-
-    public DbInitializer(TCMSContext ctx)
+    public class DbInitializer
     {
-      _ctx = ctx;
-    }
+        private readonly TCMSContext _ctx;
 
-    public void Initialize()
-    {
-      // Run Migrations
-      _ctx.Database.EnsureCreated();
-      _ctx.Database.Migrate();
+        public DbInitializer(TCMSContext ctx)
+        {
+            _ctx = ctx;
+        }
 
-      if (!_ctx.Countries.Any())
-        AddCountries();
+        public void Initialize()
+        {
+            // Run Migrations
+            _ctx.Database.EnsureCreated();
+            _ctx.Database.Migrate();
 
-      if (!_ctx.ProductGroups.Any())
-        AddProductGroups();
+            if (!_ctx.Countries.Any())
+                AddCountries();
 
-      if (!_ctx.ProductNames.Any())
-        AddProductNames();
+            if (!_ctx.ProductGroups.Any())
+                AddProductGroups();
 
-      if (!_ctx.Currencies.Any())
-        AddCurrencies();
+            if (!_ctx.ProductNames.Any())
+                AddProductNames();
 
-      if (!_ctx.CompanyTypes.Any())
-        AddCompanyTypes();
+            if (!_ctx.Currencies.Any())
+                AddCurrencies();
 
-      if (!_ctx.Locations.Any())
-        AddLocations();
+            if (!_ctx.CompanyTypes.Any())
+                AddCompanyTypes();
 
-      if (!_ctx.Regions.Any())
-        AddRegions();
+            if (!_ctx.Locations.Any())
+                AddLocations();
 
-      if (!_ctx.Banks.Any())
-        AddBanks();
+            if (!_ctx.Regions.Any())
+                AddRegions();
 
-      if (!_ctx.Logos.Any())
-        AddLogos();
+            if (!_ctx.Banks.Any())
+                AddBanks();
 
-      if (!_ctx.CorporateOffices.Any())
-        AddCorporateOffices();
+            if (!_ctx.Logos.Any())
+                AddLogos();
 
-      if (!_ctx.Roles.Any())
-        AddRoles();
+            if (!_ctx.CorporateOffices.Any())
+                AddCorporateOffices();
 
-      if (!_ctx.Users.Any())
-        AddUsers();
-    }
+            if (!_ctx.Roles.Any())
+                AddRoles();
 
-    private void AddCountries()
-    {
-      _ctx.Countries.AddRange(new List<Country>()
+            if (!_ctx.Users.Any())
+                AddUsers();
+        }
+
+        private void AddCountries()
+        {
+            _ctx.Countries.AddRange(new List<Country>()
         {
           new Country() { Name="IN", Value="India" },
           new Country() { Name="US", Value="United States" },
@@ -68,123 +68,123 @@ namespace Seaknots.TCMS.API
           new Country() { Name="FR", Value="France" },
           new Country() { Name="ES", Value="Spain" }
         });
-    }
+        }
 
-    private void AddProductGroups()
-    {
-      _ctx.ProductGroups.AddRange(new List<ProductGroup>()
+        private void AddProductGroups()
+        {
+            _ctx.ProductGroups.AddRange(new List<ProductGroup>()
         {
           new ProductGroup() { Name="PG1", Value="Product Group One" },
           new ProductGroup() { Name="PG2", Value="Product Group Two" },
           new ProductGroup() { Name="PG3", Value="Product Group Three" }
         });
-      _ctx.SaveChanges();
-    }
+            _ctx.SaveChanges();
+        }
 
-    private void AddProductNames()
-    {
-      _ctx.ProductNames.AddRange(new List<Code>()
+        private void AddProductNames()
+        {
+            _ctx.ProductNames.AddRange(new List<Code>()
         {
           new Code() { Name="PN1", Value="Product Name One" },
           new Code() { Name="PN2", Value="Product Name Two" },
           new Code() { Name="PN3", Value="Product Name Three" }
         });
-      _ctx.SaveChanges();
-    }
+            _ctx.SaveChanges();
+        }
 
-    private void AddCurrencies()
-    {
-      _ctx.Currencies.AddRange(new List<Currency>()
+        private void AddCurrencies()
+        {
+            _ctx.Currencies.AddRange(new List<Currency>()
       {
         new Currency() { Name="USD", Value="US Dollar" },
         new Currency() { Name="INR", Value="Indian Rupees" },
         new Currency() { Name="Euro", Value="Europian Euro" },
         new Currency() { Name="GBP", Value="British Pound" }
       });
-      _ctx.SaveChanges();
-    }
+            _ctx.SaveChanges();
+        }
 
-    private void AddCompanyTypes()
-    {
-      _ctx.CompanyTypes.AddRange(new List<CompanyType>()
+        private void AddCompanyTypes()
+        {
+            _ctx.CompanyTypes.AddRange(new List<CompanyType>()
       {
         new CompanyType() { Name="CT1", Value="Company Type One" },
         new CompanyType() { Name="CT2", Value="Company Type Two" },
         new CompanyType() { Name="CT3", Value="Company Type Three" }
       });
-      _ctx.SaveChanges();
-    }
+            _ctx.SaveChanges();
+        }
 
-    private void AddLocations()
-    {
-      _ctx.Locations.AddRange(new List<Location>()
+        private void AddLocations()
+        {
+            _ctx.Locations.AddRange(new List<Location>()
       {
         new Location() { Code="Loc1", Name="Location One", CountryID = 0, RegionID=0 },
         new Location() { Code="Loc2", Name="Location Two", CountryID = 0, RegionID=1 },
         new Location() { Code="Loc3", Name="Location Three", CountryID = 0, RegionID=2 }
       });
-      _ctx.SaveChanges();
-    }
+            _ctx.SaveChanges();
+        }
 
-    private void AddRegions()
-    {
-      _ctx.Regions.AddRange(new List<Region>()
+        private void AddRegions()
+        {
+            _ctx.Regions.AddRange(new List<Region>()
       {
         new Region() { Name="IN", Value="Chennai" },
         new Region() { Name="IN", Value="Delhi" },
         new Region() { Name="US", Value="Newyork" },
         new Region() { Name="US", Value="Washigton DC" }
       });
-      _ctx.SaveChanges();
-    }
+            _ctx.SaveChanges();
+        }
 
-    private void AddCorporateOffices()
-    {
-      _ctx.CorporateOffices.AddRange(new List<CorporateOffice>()
+        private void AddCorporateOffices()
+        {
+            _ctx.CorporateOffices.AddRange(new List<CorporateOffice>()
       {
         new CorporateOffice() { GlobalID="CO1", ShortName="Corporate Office 1", CompanyName="Corporate Office One", CompanyTypeID=0, CurrencyID=0, CountryID=0, LocationID=0, Email="co1@tcms.com", Address="1 Avenue, Chennai", BankID=1, LogoID=1 },
         new CorporateOffice() { GlobalID="CO2", ShortName="Corporate Office 2", CompanyName="Corporate Office Two", CompanyTypeID=1, CurrencyID=0, CountryID=0, LocationID =1, Email ="co2@tcms.com", Address="2 Avenue, Delhi", BankID=1, LogoID=1 },
         new CorporateOffice() { GlobalID="CO3", ShortName="Corporate Office 3", CompanyName="Corporate Office Three", CompanyTypeID=2, CurrencyID=1, CountryID=1, LocationID =2, Email ="co3@tcms.com", Address="3 Avenue, Newyork", BankID=1, LogoID=1 }
       });
-      _ctx.SaveChanges();
-    }
+            _ctx.SaveChanges();
+        }
 
-    private void AddRoles()
-    {
-      _ctx.Roles.AddRange(new List<Role>()
+        private void AddRoles()
+        {
+            _ctx.Roles.AddRange(new List<Role>()
       {
         new Role() { Name = "Admin" },
         new Role() { Name = "User" }
       });
-      _ctx.SaveChanges();
-    }
+            _ctx.SaveChanges();
+        }
 
-    private void AddUsers()
-    {
-      _ctx.Users.AddRange(new List<User>()
+        private void AddUsers()
+        {
+            _ctx.Users.AddRange(new List<User>()
       {
         new User() { Name="Amresh Kumar", Email="kumar.anirudha@gmail.com", Password="aks", RoleID=0, CompanyID=0, IsActive=true, LocationID=0 },
         new User() { Name="Kumar Anirudha", Email="kumar.anirudha@hotmail.com", Password="aks", RoleID=1, CompanyID=1, IsActive=true,LocationID=1 }
       });
-      _ctx.SaveChanges();
-    }
+            _ctx.SaveChanges();
+        }
 
-    private void AddBanks()
-    {
-      _ctx.Banks.AddRange(new List<BankInfo>()
+        private void AddBanks()
+        {
+            _ctx.Banks.AddRange(new List<BankInfo>()
       {
         new BankInfo()
       });
-      _ctx.SaveChanges();
-    }
+            _ctx.SaveChanges();
+        }
 
-    private void AddLogos()
-    {
-      _ctx.Logos.AddRange(new List<Logo>()
+        private void AddLogos()
+        {
+            _ctx.Logos.AddRange(new List<Logo>()
       {
         new Logo()
       });
-      _ctx.SaveChanges();
+            _ctx.SaveChanges();
+        }
     }
-  }
 }
--- master/Seaknots.TCMS/Seaknots.TCMS.API/Startup.cs
+++ master/Seaknots.TCMS/Seaknots.TCMS.API/Startup.cs
@@ -1,118 +1,117 @@
 ﻿namespace Seaknots.TCMS.API
 {
-  using Microsoft.AspNetCore.Builder;
-  using Microsoft.AspNetCore.Hosting;
-  using Microsoft.AspNetCore.Mvc;
-  using Microsoft.EntityFrameworkCore;
-  using Microsoft.Extensions.Configuration;
-  using Microsoft.Extensions.DependencyInjection;
-  using Newtonsoft.Json;
-  using Newtonsoft.Json.Serialization;
-  using Seaknots.TCMS.Core.Abstractions.EF;
-  using Seaknots.TCMS.Core.Abstractions.Trackable;
-  using Seaknots.TCMS.Core.Concrete.EF;
-  using Seaknots.TCMS.Core.Concrete.Trackable;
-  using Seaknots.TCMS.DataAccess;
-  using Seaknots.TCMS.Entities;
-  using Seaknots.TCMS.Repository;
-  using Seaknots.TCMS.Service;
-  using Swashbuckle.AspNetCore.Swagger;
-  using Contact = Swashbuckle.AspNetCore.Swagger.Contact;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Serialization;
+    using Seaknots.TCMS.Core.Abstractions.EF;
+    using Seaknots.TCMS.Core.Abstractions.Trackable;
+    using Seaknots.TCMS.Core.Concrete.EF;
+    using Seaknots.TCMS.Core.Concrete.Trackable;
+    using Seaknots.TCMS.DataAccess;
+    using Seaknots.TCMS.Entities;
+    using Seaknots.TCMS.Repository;
+    using Seaknots.TCMS.Service;
+    using Swashbuckle.AspNetCore.Swagger;
+    using Contact = Swashbuckle.AspNetCore.Swagger.Contact;
 
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
-    }
-
-    public IConfiguration Configuration { get; }
-
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddScoped<DbInitializer>();
-
-      services.AddCors();
-      services.AddDbContext<TCMSContext>(options => options.UseSqlServer(Configuration.GetConnectionString(nameof(TCMSContext))));
-      services.AddScoped<DbContext, TCMSContext>();
-      services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-      services.AddScoped<ITrackableRepository<CorporateOffice>, TrackableRepository<CorporateOffice>>();
-      services.AddScoped<ITrackableRepository<Customer>, TrackableRepository<Customer>>();
-      services.AddScoped<ITrackableRepository<Location>, TrackableRepository<Location>>();
-      services.AddScoped<ITrackableRepository<Product>, TrackableRepository<Product>>();
-      services.AddScoped<ITrackableRepository<TankAgency>, TrackableRepository<TankAgency>>();
-      services.AddScoped<ITrackableRepository<TankOperator>, TrackableRepository<TankOperator>>();
-      services.AddScoped<ITrackableRepository<Tax>, TrackableRepository<Tax>>();
-      services.AddScoped<ITrackableRepository<User>, TrackableRepository<User>>();
-      services.AddScoped<ITrackableRepository<Vendor>, TrackableRepository<Vendor>>();
-
-      services.AddScoped<IMasterRepository<CorporateOffice>, MasterRepository<CorporateOffice>>();
-      services.AddScoped<IMasterRepository<Customer>, MasterRepository<Customer>>();
-      services.AddScoped<IMasterRepository<Location>, MasterRepository<Location>>();
-      services.AddScoped<IMasterRepository<Product>, MasterRepository<Product>>();
-      services.AddScoped<IMasterRepository<TankAgency>, MasterRepository<TankAgency>>();
-      services.AddScoped<IMasterRepository<TankOperator>, MasterRepository<TankOperator>>();
-      services.AddScoped<IMasterRepository<Tax>, MasterRepository<Tax>>();
-      services.AddScoped<IMasterRepository<User>, MasterRepository<User>>();
-      services.AddScoped<IMasterRepository<Vendor>, MasterRepository<Vendor>>();
-
-      services.AddScoped<ICorporateOfficeService, CorporateOfficeService>();
-      services.AddScoped<ICustomerService, CustomerService>();
-      services.AddScoped<ILocationService, LocationService>();
-      services.AddScoped<IProductService, ProductService>();
-      services.AddScoped<ITankAgencyService, TankAgencyService>();
-      services.AddScoped<ITankOperatorService, TankOperatorService>();
-      services.AddScoped<ITaxService, TaxService>();
-      services.AddScoped<IUserService, UserService>();
-      services.AddScoped<IVendorService, VendorService>();
-
-      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
-                      .AddJsonOptions(opt =>
-                      {
-                        opt.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-                        opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-                      });
-
-      services.AddSwaggerGen(c =>
-      {
-        c.SwaggerDoc("v1", new Info
+        public Startup(IConfiguration configuration)
         {
-          Version = "v1",
-          Title = "TCMS API",
-          Description = "Seaknots TCMS API",
-          TermsOfService = "None",
-          Contact = new Contact() { Name = "Seaknots", Email = "contact@seaknots.com", Url = "www.seaknots.com" }
-        });
-      });
-    }
+            Configuration = configuration;
+        }
 
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-        app.UseCors(builder =>
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
         {
-          builder.AllowAnyOrigin();
-          builder.AllowAnyHeader();
-          builder.AllowAnyMethod();
-          builder.AllowCredentials();
-          builder.Build();
-        });
-      }
-      else
-      {
-        app.UseHsts();
-      }
+            services.AddScoped<DbInitializer>();
 
-      app.UseHttpsRedirection();
-      app.UseMvc();
-      app.UseSwagger();
-      app.UseSwaggerUI(c =>
-      {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "TCMS API V1");
-      });
+            services.AddCors();
+            services.AddDbContext<TCMSContext>(options => options.UseSqlServer(Configuration.GetConnectionString(nameof(TCMSContext))));
+            services.AddScoped<DbContext, TCMSContext>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddScoped<ITrackableRepository<CorporateOffice>, TrackableRepository<CorporateOffice>>();
+            services.AddScoped<ITrackableRepository<Customer>, TrackableRepository<Customer>>();
+            services.AddScoped<ITrackableRepository<Location>, TrackableRepository<Location>>();
+            services.AddScoped<ITrackableRepository<Product>, TrackableRepository<Product>>();
+            services.AddScoped<ITrackableRepository<TankAgency>, TrackableRepository<TankAgency>>();
+            services.AddScoped<ITrackableRepository<TankOperator>, TrackableRepository<TankOperator>>();
+            services.AddScoped<ITrackableRepository<Tax>, TrackableRepository<Tax>>();
+            services.AddScoped<ITrackableRepository<User>, TrackableRepository<User>>();
+            services.AddScoped<ITrackableRepository<Vendor>, TrackableRepository<Vendor>>();
+
+            services.AddScoped<IMasterRepository<CorporateOffice>, MasterRepository<CorporateOffice>>();
+            services.AddScoped<IMasterRepository<Customer>, MasterRepository<Customer>>();
+            services.AddScoped<IMasterRepository<Location>, MasterRepository<Location>>();
+            services.AddScoped<IMasterRepository<Product>, MasterRepository<Product>>();
+            services.AddScoped<IMasterRepository<TankAgency>, MasterRepository<TankAgency>>();
+            services.AddScoped<IMasterRepository<TankOperator>, MasterRepository<TankOperator>>();
+            services.AddScoped<IMasterRepository<Tax>, MasterRepository<Tax>>();
+            services.AddScoped<IMasterRepository<User>, MasterRepository<User>>();
+            services.AddScoped<IMasterRepository<Vendor>, MasterRepository<Vendor>>();
+
+            services.AddScoped<ICorporateOfficeService, CorporateOfficeService>();
+            services.AddScoped<ICustomerService, CustomerService>();
+            services.AddScoped<ILocationService, LocationService>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ITankAgencyService, TankAgencyService>();
+            services.AddScoped<ITankOperatorService, TankOperatorService>();
+            services.AddScoped<ITaxService, TaxService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IVendorService, VendorService>();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                            .AddJsonOptions(opt =>
+                            {
+                                opt.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                                opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "TCMS API",
+                    Description = "Seaknots TCMS API",
+                    TermsOfService = "None",
+                    Contact = new Contact() { Name = "Seaknots", Email = "contact@seaknots.com", Url = "www.seaknots.com" }
+                });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            app.UseCors(builder =>
+            {
+                builder.AllowAnyOrigin();
+                builder.AllowAnyHeader();
+                builder.AllowAnyMethod();
+                builder.AllowCredentials();
+                builder.Build();
+            });
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TCMS API V1");
+            });
+        }
     }
-  }
 }
